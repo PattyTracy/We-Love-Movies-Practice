77e7afaab6f0fb87b5a6867ae66057e46ddd8aa8
Patty Tracy <house.of.tracy@gmail.com>
2023-09-18 07:06:34 -0400

    Theaters route

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,8 +1,11 @@
 const theatersService = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-
+async function list(req, res) {
+    data = await theatersService.list();
+    res.json({ data });
+}
 
 module.exports = {
-
+    list: asyncErrorBoundary(list),
 };
