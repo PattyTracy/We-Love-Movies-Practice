ede11fd011bc7e0598fd7ced0c1a606914bb5ceb
Patty Tracy <house.of.tracy@gmail.com>
2023-09-18 08:15:31 -0400

    Theaters route take 2

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -20,36 +20,39 @@ const addMovies = reduceProperties("theater_id", {
   description: ["movies", null, "description"],
   m_created_at: ["movies", null, "movie.created_at"],
   m_updated_at: ["movies", null, "movie.updated_at"],
-  is_showing: ["movies_theaters", null, "is_showing"],
-  mt_theater_id: ["movies_theaters", null, "movies_theaters.updated_at"],
+  mt_is_showing: ["movies", null, "is_showing"],
+  mt_theater_id: ["movies", null, "theater_id"],
 })
 
 function list() {
   return knex("theaters as t")
-  .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
-  .join("movies as m", "mt.movie_id", "m.movie_id")
-  .select(
-    "t.theater_id",
-    "t.name",
-    "t.address_line_1",
-    "t.address_line_2",
-    "t.city",
-    "t.state",
-    "t.zip",
-    "t.created_at",
-    "t.updated_at",
-    "m.movie_id",
-    "m.title",
-    "m.runtime_in_minutes",
-    "m.rating",
-    "m.description",
-    "m.image_url",
-    "m.created_at as m_created_at",
-    "m.updated_at as m_updated_at",
-    "mt.is_showing",
-    "mt.theater_id as mt_theater_id",
-    )
-    .then(addMovies);
+  .select("theater_id", "name as theater_name")
+  .leftJoin("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+  .leftJoin("movies as m", "mt.movie_id", "m.movie_id")
+  .groupBy("t.theater_id", "t.name")
+  .orderBy("t.theater_id")
+  .groupByRaw("json_agg(json_build_object(\'id\', movies.id, \'title\', movies.title, \'release_date\', movies.release_date, \'genre\', movies.genre)) as movies")
+
+
+//   return knex("theaters as t")
+//   .leftJoin("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+//   .rightJoin("movies as m", "mt.movie_id", "m.movie_id")
+//   .select(
+//     "t.*",
+//     "m.movie_id",
+//     "m.title",
+//     "m.runtime_in_minutes",
+//     "m.rating",
+//     "m.description",
+//     "m.image_url",
+//     "m.created_at as m_created_at",
+//     "m.updated_at as m_updated_at",
+//     "mt.is_showing as mt_is_showing",
+//     "mt.theater_id as mt_theater_id",
+//     )
+//     // 
+//     .groupBy('t.theater_id')
+//     .then(addMovies);
 }
 
 module.exports = {
