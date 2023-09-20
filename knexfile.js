82dcbb1b1b16fdd4fa785a1541612429b7b54058
Patty Tracy <house.of.tracy@gmail.com>
2023-09-19 20:09:10 -0400

    Revise connection

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://iivcesay:Vs9sJDtoP7XhB0M_AwPjxhf9-YB6hiBS@berry.db.elephantsql.com/iivcesay"
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
