f4ae28cd24faf8903cab5646e23844c44da9a6a7
Patty Tracy <house.of.tracy@gmail.com>
2023-09-21 05:45:42 -0400

    Revise connections

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const app = express();
 
 const notFound = require("./errors/notFound");
-const app = express();
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
